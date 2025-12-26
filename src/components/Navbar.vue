17d2d2715fde3b778e724ee053ca9f206682c757
OmarHossam50 <omarhossam50@proton.me>
2025-12-26 21:11:52 +0200

    feat: implement basic Navbar, load fonts and images

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Logo from "@images/logo-small.svg"
+import Trophy from "@images/icon-personal-best.svg"
+import { ref } from 'vue'
+
+const score = ref(92)
+  
+</script>
+<template>
+  <header>
+    <nav>
+      <ul>
+	<li>
+	  <a href="/">
+	    <div>
+	      <img :src="Logo" />
+	    </div>
+	    <div>
+	      <h1>Typing Speed Test</h1>
+	      <p>Type as fast as you can in 60 seconds</p>
+	    </div>
+	  </a>
+	</li>
+	
+	<li>
+	  <div>
+	    <img :src="Trophy" />
+	  </div>
+	  <p>Personal best: <strong>{{ score }}</strong></p>
+	</li>
+      </ul>
+    </nav>
+  </header>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@images': fileURLToPath(new URL('./src/assets/images', import.meta.url))
     },
   },
 })
