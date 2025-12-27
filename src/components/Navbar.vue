34d034286e5d20920362c4ea4c1a92c912019ffa
OmarHossam50 <omarhossam50@proton.me>
2025-12-27 18:23:50 +0200

    feat: implement Navbar for desktop screens and add padding for the entire website

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,24 +9,22 @@ const score = ref(92)
 <template>
   <header>
     <nav>
-      <ul>
-	<li>
-	  <a href="/">
-	    <div>
-	      <img :src="Logo" />
-	    </div>
-	    <div>
-	      <h1>Typing Speed Test</h1>
-	      <p>Type as fast as you can in 60 seconds</p>
-	    </div>
-	  </a>
+      <ul class="flex justify-between items-center">
+	<li class="flex gap-2 select-none">
+	  <div>
+	    <img :src="Logo" alt="keyboard image" class="w-12 h-full object-center object-cover" />
+	  </div>
+	  <div>
+	    <h1 class="font-bold text-3xl pb-[2px]">Typing Speed Test</h1>
+	    <p class="text-sm text-slate-700">Type as fast as you can in 60 seconds</p>
+	  </div>
 	</li>
 	
-	<li>
+	<li class="flex items-center gap-2">
 	  <div>
 	    <img :src="Trophy" />
 	  </div>
-	  <p>Personal best: <strong>{{ score }}</strong></p>
+	  <p class="text-slate-700">Personal best: <strong class="text-black">{{ score }}</strong></p>
 	</li>
       </ul>
     </nav>
