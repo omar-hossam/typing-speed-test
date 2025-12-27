5ef7575aa93fc11e3d71d35f14084d60035135c6
OmarHossam50 <omarhossam50@proton.me>
2025-12-28 00:19:14 +0200

    feat: implement basics of RestartTestBtn and update App.vue with new components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
   import Navbar from "@components/Navbar.vue";
   import Bar from "@components/Bar.vue";
+  import TestSection from "@components/TestSection.vue";
+  import RestartTestBtn from "@components/RestartTestBtn.vue";
 </script>
 
 <template>
   <Navbar />
-  <Bar />
+  <main class="flex flex-col">
+    <Bar />
+    <hr class="border border-slate-200 rounded-lg" />
+    <TestSection />
+    <hr class="border border-slate-200 rounded-lg" />
+    <RestartTestBtn />
+  </main>
 </template>
 
 <style scoped></style>
